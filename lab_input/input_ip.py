ab3b18ca83e590be6eb9b609d13c5a9b14cc2356
Michael Hagenlocker <michael.hagenlocker@vzw.com>
2018-11-06 14:16:10 -0600

    Initial commit.

---
+++ lab_input/input_ip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Written By: Alice Anderson
+# Collect IP address information from a user, and display it back to them.
+
+## Collect input from the user
+user_input = input('Please enter an IPv4 IP address:') # user_input stores the IPv4 ip address
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
+
+## Collect vendor name from the user
+vendor_name = input('Please enter a vendor name:')
+
+## Print out the vendor name collected from the user
+print("You told me the vendor name is:" + vendor_name)
