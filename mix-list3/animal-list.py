2aa897d5cc4fbdb653f381b1de05d801f694f54a
Michael Hagenlocker <michael.hagenlocker@vzw.com>
2018-11-07 13:33:38 -0600

    Lab 12

---
+++ mix-list3/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+animals = 'Fox Fly Ant Bee cod Cat Dog Yak Cow Hen Koi Hog Jay Kit'
+print(animals)
+animal_list = animals.split()
+print(animal_list)
+print(', '.join(animal_list))
+print('\n'.join(animal_list))
